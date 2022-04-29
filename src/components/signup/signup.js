deb301c175c6011854a88156c2d8ad193d2573d4
Jone <dioneishikawa@gmail.com>
2022-04-29 19:53:54 -0300

    Yup installation and signup validation schema creation

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -8,10 +8,13 @@ import {
   Button,
   InputGroup,
   InputRightElement,
-  Box
+  Box,
+  FormErrorMessage
 } from "@chakra-ui/react"
 import { motion } from "framer-motion"
 import { useTranslation } from "react-i18next"
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import * as Yup from 'yup'
 
 import BackIcon from '../../assets/icons/back.svg'
 import OpenEye from '../../assets/icons/open_eye.svg'
@@ -20,13 +23,25 @@ import ClosedEye from '../../assets/icons/closed_eye.svg'
 const SignUp = ({type, setType}) => {
   const {t, i18n} = useTranslation()
 
-  const [username, setUsername] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-  const [passwordConfirmation, setPasswordConfirmation] = useState()
   const [securePass, setSecurePass] = useState(true)
   const [secureConfirm, setSecureConfirm] = useState(true)
-  const [isLoading, setIsLoading] = useState(false)
+
+  const ValidationSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(3, t('minUsernameChar'))
+      .max(24, t('maxUsernameChar'))
+      .required(t('requiredField')),
+    email: Yup.string()
+      .email(t('invalidEmail'))
+      .required(t('requiredField')),
+    password: Yup.string()
+      .min(8, t('minPassword'))
+      .max(16, t('maxPassword'))
+      .matches(/^[a-zA-Z0-9@*#]{8,16}$/, t('invalidPassword'))
+      .required(t('requiredField')),
+    passwordConfirmation: Yup.string()
+      .oneOf([Yup.ref('password'), null], t('passwordsMustMatch'))
+  })
 
   const animationKeyframes = keyframes`
     0% {opacity: 0.3;}
@@ -37,23 +52,6 @@ const SignUp = ({type, setType}) => {
   const animationB = `${animationKeyframes} 4s`;
   const animationC = `${animationKeyframes} 6s`;
   const animationD = `${animationKeyframes} 8s`;
-
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value)
-  }
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value)
-  }
-
-  const handlePasswordConfirmationChange = (e) => {
-    setPasswordConfirmation(e.target.value)
-  }
 
   const handleSignUp = () => {
 
@@ -86,7 +84,7 @@ const SignUp = ({type, setType}) => {
         height={'50px'}
         alignItems={'center'}
         justifyContent={'center'}
-        paddingVertical={'15px'}>
+        py={'15px'}>
           <Box
           width={'65%'}
           height={'75%'}
@@ -106,180 +104,235 @@ const SignUp = ({type, setType}) => {
       </Flex>
 
       <FormControl alignSelf={'center'}>
-
-        <Flex
-        pt={'20px'}
-        as={motion.div}
-        animation={animationA}
-        width={'100%'}
-        alignItems={'center'}
-        direction={'column'}
-        mb={'10px'}>
-          <Flex width={'70%'} alignItems={'flex-start'}>
-            <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
-              {t('username')}
-            </Text>
-          </Flex>
-          <Input
-          value={username}
-          onChange={(e) => handleUsernameChange(e)}
-          textColor={'bg.200'}
-          px={'5px'}
-          variant={'unstyled'}
-          borderRadius={0}
-          borderWidth={'4px'}
-          borderColor={'bg.200'}
-          width={'70%'}
-          height={{base: '40px', 'md': '50px'}} />
-        </Flex>
-
-        <Flex
-        pt={'10px'}
-        as={motion.div}
-        animation={animationB}
-        width={'100%'}
-        alignItems={'center'}
-        direction={'column'}
-        mb={'10px'}>
-          <Flex width={'70%'} alignItems={'flex-start'}>
-            <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
-              {t('email')}
-            </Text>
-          </Flex>
-          <Input
-          onError={(e) => console.log(e)}
-          type={'email'}
-          value={email}
-          onChange={(e) => handleEmailChange(e)}
-          textColor={'bg.200'}
-          px={'5px'}
-          variant={'unstyled'}
-          borderRadius={0}
-          borderWidth={'4px'}
-          borderColor={'bg.200'}
-          width={'70%'}
-          height={{base: '40px', 'md': '50px'}} />
-        </Flex>
-
-        <Flex
-        pt={'10px'}
-        as={motion.div}
-        animation={animationC}
-        width={'100%'}
-        alignItems={'center'}
-        direction={'column'}
-        mb={'10px'}>
-          <Flex width={'70%'} alignItems={'flex-start'}>
-            <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
-              {t('password')}
-            </Text>
-          </Flex>
-          <InputGroup
-          variant={'unstyled'}
-          borderRadius={0}
-          borderWidth={'4px'}
-          borderColor={'bg.200'}
-          width={'70%'}
-          height={{base: '40px', 'md': '50px'}} >
-            <Input
-            textColor={'bg.200'}
-            value={password}
-            onChange={(e) => handlePasswordChange(e)}
-            px={'5px'}
-            type={securePass ? 'password' : 'text'} />
-              <InputRightElement width={{base: '3.5rem', 'md': '4.5'}}>
-                <Button
-                left={'5px'}
-                top={'-3px'}
-                size='sm'
-                width={'3rem'}
-                height={{base: '2.5rem', 'md': '3rem'}}
-                variant={'ghost'} 
-                onClick={() => setSecurePass(!securePass)}>
+        <Formik
+        initialValues={{
+          username: '',
+          email: '',
+          password: '',
+          passwordConfirmation: '',
+        }}
+        valitadionSchema={ValidationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            console.log(values)
+            setSubmitting(false);
+          }, 400);
+        }}
+        >
+          {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+          }) => (
+            <Form>
+              <Flex direction={'column'}>
+                <Flex
+                pt={'20px'}
+                as={motion.div}
+                animation={animationA}
+                width={'100%'}
+                alignItems={'center'}
+                direction={'column'}
+                mb={'10px'}>
+                  <Flex width={'70%'} alignItems={'flex-start'}>
+                    <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
+                      {t('username')}
+                    </Text>
+                  </Flex>
+                  <Input
+                  id='username'
+                  name='username'
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  textColor={'bg.200'}
+                  px={'5px'}
+                  variant={'unstyled'}
+                  borderRadius={0}
+                  borderWidth={'4px'}
+                  borderColor={'bg.200'}
+                  width={'70%'}
+                  height={{base: '40px', 'md': '50px'}} />
                   {
-                    securePass ?
-                    <OpenEye /> :
-                    <ClosedEye />
+                    errors.username && touched.username ?
+                    <Flex width={'100%'} height={'50px'} name='username' >
+                      {errors.username}
+                    </Flex>
+                    : null
                   }
-                </Button>
-              </InputRightElement>
-          </InputGroup>
-        </Flex>
+                </Flex>
 
-        <Flex
-        pt={'10px'}
-        as={motion.div}
-        animation={animationD}
-        width={'100%'}
-        alignItems={'center'}
-        direction={'column'}
-        mb={'10px'}>
-          <Flex width={'70%'} alignItems={'flex-start'}>
-            <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
-              {t('confirmPassword')}
-            </Text>
-          </Flex>
-          <InputGroup
-          variant={'unstyled'}
-          borderRadius={0}
-          borderWidth={'4px'}
-          borderColor={'bg.200'}
-          width={'70%'}
-          height={{base: '40px', 'md': '50px'}} >
-            <Input
-            textColor={'bg.200'}
-            value={passwordConfirmation}
-            onChange={(e) => handlePasswordConfirmationChange(e)}
-            px={'5px'}
-            type={secureConfirm ? 'password' : 'text'} />
-              <InputRightElement width={{base: '3.5rem', 'md': '4.5'}}>
-                <Button
-                left={'5px'}
-                top={'-3px'}
-                size='sm'
-                width={'3rem'}
-                height={{base: '2.5rem', 'md': '3rem'}}
-                variant={'ghost'} 
-                onClick={() => setSecureConfirm(!secureConfirm)}>
-                  {
-                    secureConfirm ?
-                    <OpenEye /> :
-                    <ClosedEye />
-                  }
-                </Button>
-              </InputRightElement>
-          </InputGroup>
-        </Flex>
+                <Flex
+                pt={'10px'}
+                as={motion.div}
+                animation={animationB}
+                width={'100%'}
+                alignItems={'center'}
+                direction={'column'}
+                mb={'10px'}>
+                  <Flex width={'70%'} alignItems={'flex-start'}>
+                    <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
+                      {t('email')}
+                    </Text>
+                  </Flex>
+                  <Input
+                  id='email'
+                  name='email'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  type={'email'}
+                  value={values.email}
+                  textColor={'bg.200'}
+                  px={'5px'}
+                  variant={'unstyled'}
+                  borderRadius={0}
+                  borderWidth={'4px'}
+                  borderColor={'bg.200'}
+                  width={'70%'}
+                  height={{base: '40px', 'md': '50px'}} />
+                  <Text fontSize={'1rem'}>
+                    {
+                      errors.email && touched.email ?
+                      <Text>
+                        erou
+                      </Text>
+                      : null
+                    }
+                  </Text>
+                  
+                </Flex>
 
-        <Flex
-        mt={'25px'}
-        mb={'15px'}
-        alignItems={'center'}
-        justifyContent={'center'} >
-          <Button
-          onClick={handleSignUp}
-          backgroundColor={'bg.100'}
-          colorScheme={'blackAlpha'}
-          isLoading={isLoading}
-          alignItems={'center'}
-          justifyContent={'center'}
-          borderRadius={0}
-          borderWidth={'4px'}
-          borderColor={'bg.200'}
-          height={'50px'}
-          width={'70%'}
-          _hover={{
-            bg: '#FFFFF9'
-          }} >
-            <Text
-            fontWeight={'600'}
-            fontSize={'1.3rem'}
-            color={'bg.200'}>
-              {t('signup')}
-            </Text>
-          </Button>
-        </Flex>
+                <Flex
+                pt={'10px'}
+                as={motion.div}
+                animation={animationC}
+                width={'100%'}
+                alignItems={'center'}
+                direction={'column'}
+                mb={'10px'}>
+                  <Flex width={'70%'} alignItems={'flex-start'}>
+                    <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
+                      {t('password')}
+                    </Text>
+                  </Flex>
+                  <InputGroup
+                  variant={'unstyled'}
+                  borderRadius={0}
+                  borderWidth={'4px'}
+                  borderColor={'bg.200'}
+                  width={'70%'}
+                  height={{base: '40px', 'md': '50px'}} >
+                    <Input
+                    id='password'
+                    name='password'
+                    textColor={'bg.200'}
+                    value={values.password}
+                    onChange={handleChange}
+                    px={'5px'}
+                    type={securePass ? 'password' : 'text'} />
+                      <InputRightElement width={{base: '3.5rem', 'md': '4.5'}}>
+                        <Button
+                        left={'5px'}
+                        top={'-3px'}
+                        size='sm'
+                        width={'3rem'}
+                        height={{base: '2.5rem', 'md': '3rem'}}
+                        variant={'ghost'} 
+                        onClick={() => setSecurePass(!securePass)}>
+                          {
+                            securePass ?
+                            <OpenEye /> :
+                            <ClosedEye />
+                          }
+                        </Button>
+                      </InputRightElement>
+                  </InputGroup>
+                </Flex>
 
+                <Flex
+                pt={'10px'}
+                as={motion.div}
+                animation={animationD}
+                width={'100%'}
+                alignItems={'center'}
+                direction={'column'}
+                mb={'10px'}>
+                  <Flex width={'70%'} alignItems={'flex-start'}>
+                    <Text fontWeight={'600'} color={'bg.200'} fontSize={'1rem'}>
+                      {t('confirmPassword')}
+                    </Text>
+                  </Flex>
+                  <InputGroup
+                  variant={'unstyled'}
+                  borderRadius={0}
+                  borderWidth={'4px'}
+                  borderColor={'bg.200'}
+                  width={'70%'}
+                  height={{base: '40px', 'md': '50px'}} >
+                    <Input
+                    id='passwordConfirmation'
+                    name='passwordConfirmation'
+                    textColor={'bg.200'}
+                    value={values.passwordConfirmation}
+                    onChange={handleChange}
+                    px={'5px'}
+                    type={secureConfirm ? 'password' : 'text'} />
+                      <InputRightElement width={{base: '3.5rem', 'md': '4.5'}}>
+                        <Button
+                        left={'5px'}
+                        top={'-3px'}
+                        size='sm'
+                        width={'3rem'}
+                        height={{base: '2.5rem', 'md': '3rem'}}
+                        variant={'ghost'} 
+                        onClick={() => setSecureConfirm(!secureConfirm)}>
+                          {
+                            secureConfirm ?
+                            <OpenEye /> :
+                            <ClosedEye />
+                          }
+                        </Button>
+                      </InputRightElement>
+                  </InputGroup>
+                </Flex>
+                <Flex
+                mt={'25px'}
+                mb={'15px'}
+                alignItems={'center'}
+                justifyContent={'center'} >
+                  <Button
+                  type='submit'
+                  onClick={handleSubmit}
+                  backgroundColor={'bg.100'}
+                  colorScheme={'blackAlpha'}
+                  isLoading={isSubmitting}
+                  alignItems={'center'}
+                  justifyContent={'center'}
+                  borderRadius={0}
+                  borderWidth={'4px'}
+                  borderColor={'bg.200'}
+                  height={'50px'}
+                  width={'70%'}
+                  _hover={{
+                    bg: '#FFFFF9'
+                  }} >
+                    <Text
+                    fontWeight={'600'}
+                    fontSize={'1.3rem'}
+                    color={'bg.200'}>
+                      {t('signup')}
+                    </Text>
+                  </Button>
+                </Flex>
+              </Flex>
+            </Form>
+          )}
+        </Formik>
       </FormControl>
       
     </Flex>
--- src/translation/i18n.js
+++ src/translation/i18n.js
@@ -11,7 +11,15 @@ const resources = {
       "login": "LOGIN",
       "signup": "SIGN UP",
       "username": "NAME",
-      "confirmPassword": "CONFIRM YOUR PASSWORD"
+      "confirmPassword": "CONFIRM YOUR PASSWORD",
+      "minUsernameChar": "Your username is too short :(",
+      "maxUsernameChar": "Your username is too long :(",
+      "invalidEmail": "Invalid e-mail.",
+      "minPassword": "Your password must have at least 8 characters.",
+      "maxPassword": "Your password can't have more than 16 characters",
+      "invalidPassword": "Password requires at one lower case letter, one upper case letter, one digit, 8-16 length, no spaces and one symbol(# @ *).",
+      "passwordsMustMatch": "Passwords must match.",
+      "requiredField": "This filed is required.",
     }
   },
   pt: {
@@ -22,7 +30,15 @@ const resources = {
       "login": "ENTRAR",
       "signup": "CADASTRAR-SE",
       "username": "NOME",
-      "confirmPassword": "CONFIRME SUA SENHA"
+      "confirmPassword": "CONFIRME SUA SENHA",
+      "minUsernameChar": "Nome de usuário muito curto :(",
+      "maxUsernameChar": "Seu nome de usuário está muito longo :(",
+      "invalidEmail": "E-mail inválido",
+      "minPassword": "Sua senha deve conter pelo menos 8 caracteres",
+      "maxPassword": "Sua deve ter menos de 16 caracteres",
+      "invalidPassword": "Sua senha não pode conter espaços e deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito, entre 8 e 16 caracteres e um símbolo(@ # *).",
+      "passwordsMustMatch": "A senhas devem ser iguais",
+      "requiredField": "Este campo é obrigatório"
     }
   }
 };
